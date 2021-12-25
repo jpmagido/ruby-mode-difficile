14a1ce1583f4845e98e0e55b3da0e746c7b7b02b
jpmagido <jpmagido@me.com>
2021-12-25 11:53:24 +0100

    oauth redirect + procfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/contact'
+  get 'oauth-redirect', to: 'static_pages#oauth'
 
   resources :challenges, only: %i[index show new create]
 end
