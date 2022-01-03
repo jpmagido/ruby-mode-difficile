5eecc2d62adc67e024e1b7708726ece76d376ba6
jpmagido <jpmagido@me.com>
2022-01-03 15:18:22 +0100

    user_session_spec

---
+++ spec/services/github/sync/user_session_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Github::Sync::UserSession, type: :service do
+  subject(:sync_user_session) { described_class.new(token, request) }
+
+  let(:token) { SecureRandom.hex 10 }
+  let(:request) { Struct.new(:remote_ip) }
+
+  describe '#new' do
+    it 'initializes properly' do
+      expect { sync_user_session }.not_to raise_error
+    end
+  end
+
+  describe '#save_session!' do
+    before do
+      allow_any_instance_of(Github::Api).to receive(:find_user).and_return id: 123
+    end
+
+    it { expect { sync_user_session.save_session! }.to change(User, :count).by 1 }
+    it { expect { sync_user_session.save_session! }.to change(Session, :count).by 1 }
+    it { expect(sync_user_session.save_session!).to eq Session.last.id }
+  end
+
+  context 'errors' do
+    it { expect { described_class.new(token, nil) }.to raise_error ArgumentError }
+    it { expect { described_class.new(nil, request) }.to raise_error ArgumentError }
+  end
+end
