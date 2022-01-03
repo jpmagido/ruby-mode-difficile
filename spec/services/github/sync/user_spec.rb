7776fc2b49b273e00cdd10ac3be76a4cbe9eba5c
jpmagido <jpmagido@me.com>
2022-01-03 15:32:50 +0100

    rspec github sync user

---
+++ spec/services/github/sync/user_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Github::Sync::User, type: :service do
+  subject(:sync_user) { described_class.new(github_user) }
+
+  let(:github_user) do
+    { id: 123,
+      login: 'my custom name',
+      bio: 'I am tall',
+      email: 'rspec@rspec.com',
+      repos_url: 'https://www.my_custom_url.com',
+      avatar_url: 'https://www.my_avatar_url.com',
+      blog: 'https://www.my_blog_url.com',
+      followers: 10
+    }
+
+  end
+
+  describe '#new' do
+    it { expect { sync_user }.not_to raise_error }
+  end
+
+  describe '#synced_user' do
+    it { expect(sync_user.synced_user).to be_a User }
+    it { expect(sync_user.synced_user.github_id).to eq 123 }
+    it { expect(sync_user.synced_user.login).to eq 'my custom name' }
+    it { expect(sync_user.synced_user.bio).to eq 'I am tall' }
+    it { expect(sync_user.synced_user.email).to eq 'rspec@rspec.com' }
+    it { expect(sync_user.synced_user.repos_url).to eq 'https://www.my_custom_url.com' }
+    it { expect(sync_user.synced_user.avatar_url).to eq 'https://www.my_avatar_url.com' }
+    it { expect(sync_user.synced_user.blog).to eq 'https://www.my_blog_url.com' }
+    it { expect(sync_user.synced_user.followers).to eq 10 }
+  end
+
+  context 'when wrong inputs' do
+    it { expect { described_class.new(id: nil) }.to raise_error ArgumentError }
+    it { expect { described_class.new(followers: 10) }.to raise_error ArgumentError }
+  end
+end
