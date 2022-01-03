d4e9dbb3312fcf3940a319e89bd1c9889d955fd2
jpmagido <jpmagido@me.com>
2022-01-03 15:20:20 +0100

    minor changes

--- spec/services/github/sync/user_session_spec.rb
+++ spec/services/github/sync/user_session_spec.rb
@@ -9,22 +9,18 @@ RSpec.describe Github::Sync::UserSession, type: :service do
   let(:request) { Struct.new(:remote_ip) }
 
   describe '#new' do
-    it 'initializes properly' do
-      expect { sync_user_session }.not_to raise_error
-    end
+    it { expect { sync_user_session }.not_to raise_error }
   end
 
   describe '#save_session!' do
-    before do
-      allow_any_instance_of(Github::Api).to receive(:find_user).and_return id: 123
-    end
+    before { allow_any_instance_of(Github::Api).to receive(:find_user).and_return id: 123 }
 
     it { expect { sync_user_session.save_session! }.to change(User, :count).by 1 }
     it { expect { sync_user_session.save_session! }.to change(Session, :count).by 1 }
     it { expect(sync_user_session.save_session!).to eq Session.last.id }
   end
 
-  context 'errors' do
+  context 'when wrong inputs' do
     it { expect { described_class.new(token, nil) }.to raise_error ArgumentError }
     it { expect { described_class.new(nil, request) }.to raise_error ArgumentError }
   end
