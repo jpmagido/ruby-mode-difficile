b50aaf2a11cac0bbe1b4b3573ee28319d393f58a
jpmagido <jpmagido@me.com>
2022-01-04 02:28:06 +0100

    more specs got session request

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'SessionsController', type: :request do
@@ -18,9 +20,14 @@ RSpec.describe 'SessionsController', type: :request do
   end
 
   describe 'POST /create' do
-    xit 'redirects to user show' do
-      expect(post(session_path, params: {})).to redirect_to user_path
-      #expect(response).to have_http_status(:success)
-    end
+    it { expect(post(session_path)).to redirect_to login_user_path }
+    it { expect { post(session_path) }.to change(Session, :count).by 1 }
+  end
+
+  describe 'DELETE /destroy' do
+    before { post session_path }
+
+    it { expect(delete(session_path)).to redirect_to root_path }
+    it { expect { delete(session_path) }.to change(Session, :count).by(-1) }
   end
 end
