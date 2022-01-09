8a6683c6b3d6f8199db9c558f410c14167aee236
seven-dev7 <seven.tdr7@gmail.com>
2022-01-09 19:06:55 +0100

    try to make the call

--- app/services/github/api.rb
+++ app/services/github/api.rb
@@ -23,7 +23,23 @@ module Github
     end
 
     class Repository
-      
+      attr_reader :token, :cloud_storage
+
+      GET_README_URL = 'https://api.github.com/user/repo/readme/dir'
+
+      def initialize(token:, cloud_storage:)
+        @token = token
+        @cloud_storage = cloud_storage
+
+        raise ArgumentError, 'token must be present' unless token
+        raise ArgumentError, 'url github must be present' unless cloud_storage
+      end
+
+      def get_readme
+        response = HttpService.new(GET_README_URL, {}, 'Authorization': "token #{token}").readme
+        p JSON.parse response.body
+        #JSON.parse response.body
+      end
     end
   end
 end
