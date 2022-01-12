abaf6b0e41a8ec09d80d614e4b9ff73ddfae743a
jpmagido <jpmagido@me.com>
2022-01-12 17:10:16 +0100

    install pundit default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include ::Pundit::Authorization
+
   before_action :set_locale
 
   helper_method :current_user
