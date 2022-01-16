7cbda594889dfe2fe9ccbd39f943aebcc0b2c844
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2022-01-16 16:07:06 +0100

    fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,23 @@
-# require 'factory_bot_rails'
+# frozen_string_literal: true
 
 def create_users
-    FactoryBot.create_list(:user, 10)
-    puts '10 users are created'    
+  FactoryBot.create_list(:user, 10)
+  puts '10 users are created'    
 end
 
 def create_challenges
-    challenges = FactoryBot.create_list(:challenge, 10)
-    challenges.each do |challenge|
-        for i in 0..2 do
-            FactoryBot.create(:answer,challenge_id: challenge.id)
-        end
+  challenges = FactoryBot.create_list(:challenge, 10)
+  challenges.each do |challenge|
+    3.times do
+      FactoryBot.create(:answer,challenge_id: challenge.id)
     end
-    puts '10 challenges created with each 3 answers'        
+  end
+  puts '10 challenges created with each 3 answers'        
 end
 
 def perform
-    create_users
-    create_challenges
+  create_users
+  create_challenges
 end
-
 
 perform
