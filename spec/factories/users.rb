1c2edd3692894cc7eade8e962d357da7f25bbc9c
jpmagido <jpmagido@me.com>
2022-01-21 14:59:53 +0100

    add links to mentorship in coach dashboard

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :user do
     github_id { rand(100..10000) }
-    login { FFaker::Book.title }
+    login { FFaker::NameFR.first_name }
     email { FFaker::Internet.email }
     bio { FFaker::Book.description }
     html_url { FFaker::Internet.http_url }
