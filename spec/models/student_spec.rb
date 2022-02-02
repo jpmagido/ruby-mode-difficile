70bb1c84ca40bf804e28e7bbd55933c8b11f2f63
jpmagido <jpmagido@me.com>
2022-02-02 01:56:32 +0100

    last changes

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:student) { create(:student) }
+
+  it { expect(:student).to be_valid }
 end
