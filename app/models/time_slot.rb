4f0b5369a5d1f9652d14e6052f73933f9850742f
jpmagido <jpmagido@me.com>
2022-02-16 23:18:56 +0100

    hotwire calendar + spec fix

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -3,6 +3,11 @@
 class TimeSlot < ApplicationRecord
   belongs_to :mentorship_session
 
+  after_update_commit lambda {
+    broadcast_replace_to mentorship_session,
+                         partial: 'mentor/time_slots/time_slot'
+  }
+
   def approved
     coach_approval && student_approval
   end
