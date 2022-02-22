1446c76da513389e2acb171ca65010ae3501b9db
jpmagido <jpmagido@me.com>
2022-02-22 12:36:19 +0100

    fix endlines & specs

--- spec/requests/academy/time_slots_controller_spec.rb
+++ spec/requests/academy/time_slots_controller_spec.rb
@@ -13,9 +13,11 @@ RSpec.describe 'Academy::TimeSlotsController', type: :request do
   end
 
   describe 'PATCH /update' do
+    let(:update_params) { { time_slot: { student_approval: true } } }
+
     it 'updates student_approval' do
       expect(time_slot.student_approval).to be_falsey
-      patch academy_mentorship_session_time_slot_path(time_slot.mentorship_session, time_slot, student_approval: true)
+      patch academy_mentorship_session_time_slot_path(time_slot.mentorship_session, time_slot), params: update_params
       expect(time_slot.reload.student_approval).to be_truthy
     end
   end
