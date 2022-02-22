a831903d07b0487cd02784a9e846377ae6472bd8
jpmagido <jpmagido@me.com>
2022-02-22 04:03:18 +0100

    refacto academy views + fix hotwire multiple time slots partials

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -4,8 +4,11 @@ class TimeSlot < ApplicationRecord
   belongs_to :mentorship_session
 
   after_update_commit lambda {
-    broadcast_replace_to mentorship_session,
+    broadcast_replace_to "mentor_mentorship_session_#{mentorship_session.id}",
                          partial: 'mentor/time_slots/time_slot'
+
+    broadcast_replace_to "student_mentorship_session_#{mentorship_session.id}",
+                         partial: 'academy/time_slots/time_slot'
   }
 
   def approved
