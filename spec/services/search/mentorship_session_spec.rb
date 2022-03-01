36f7781ab74f1193a88224ac844cd5ef204114c2
jpmagido <jpmagido@me.com>
2022-03-01 17:43:55 +0100

    add spec on search services

---
+++ spec/services/search/mentorship_session_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Search::MentorshipSession, type: :service do
+  let(:search_mentorship_session) { described_class.new(MentorshipSession.all, {}) }
+
+  it { expect { search_mentorship_session }.not_to raise_error }
+  it { expect(search_mentorship_session.search).to be_an ActiveRecord::Relation }
+
+  context 'Search with parameters' do
+    let(:mentorship_session) { create(:mentorship_session, start_date: DateTime.now, end_date: DateTime.now + 1) }
+    let(:outdated_mentorship_session_1) { create(:mentorship_session, start_date: DateTime.now, end_date: DateTime.now + 3) }
+    let(:outdated_mentorship_session_2) { create(:mentorship_session, start_date: DateTime.now - 3, end_date: DateTime.now + 1) }
+
+    let(:search) { described_class.new(MentorshipSession.all, params) }
+    let(:params) { { start_date: DateTime.now - 2, end_date: DateTime.now + 2 } }
+
+    it 'includes proper Mentorship Sessions' do
+      expect(search.search).to include(mentorship_session)
+      expect(search.search).not_to include(outdated_mentorship_session_1)
+      expect(search.search).not_to include(outdated_mentorship_session_2)
+    end
+  end
+end
